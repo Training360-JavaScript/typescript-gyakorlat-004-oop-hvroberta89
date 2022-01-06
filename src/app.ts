74c5a376d5797dde95f3057a9753fb7511b4487f
hvroberta89 <veber.roberta@gmail.com>
2022-01-06 23:59:54 +0100

    completed task

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import { HumanHero } from './hero';
+import { TransformerHero } from './transformer';
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    new HumanHero(0, "Nedun", "female", 30, 25),
+    new HumanHero(4, "Rebus", "female", 34, 42),
+    new HumanHero(17, "Zsir", "male", 25, 51)
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    new TransformerHero(2, "Vasmarok", 4, 2, "roborock"),
+    new TransformerHero(3, "Rézfácán", 2, 0, "hópehely"),
+    new TransformerHero(6, "Alubaba", 8, 4, "vonósok")
+];
